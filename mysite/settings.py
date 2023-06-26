2df20527598b8158c80c4b043e245d7d726c1469
Jeong Taek <imjt9345@naver.com>
2023-06-26 11:43:36 +0900

    bookmark_detail.html, bookmark_list.html 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,7 +57,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            # os.path.join(BASE_DIR, 'templates'),
+            # 'bookmark/templates/bookmark_list',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
