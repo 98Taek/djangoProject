754c22791440092444d068368992ecba51d32564
Jeong Taek <imjt9345@naver.com>
2023-06-28 09:18:41 +0900

    view.py disqus 설정 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATICFILES_DIRS = [BASE_DIR / 'static', ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -132,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DISQUS_SHORTNAME = 'django-xufobh97op'
+DISQUS_MY_DOMAIN = 'http://127.0.0.1:8000'
